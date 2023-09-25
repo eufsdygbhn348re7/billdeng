977fb4bab45590db1a9ddf7a675f3ce880456096
eufsdygbhn348re7 <106133348+eufsdygbhn348re7@users.noreply.github.com>
2023-09-25 06:38:41 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='ee86e666-4f93-4a8d-96f6-b63344d9a476'
-VMESS_WSPATH='/ee86e666-4f93-4a8d-96f6-b63344d9a476-vmess'
-VLESS_WSPATH='/ee86e666-4f93-4a8d-96f6-b63344d9a476-vless'
+UUID='4f2dee67-bc5a-435f-9667-4dc8c1ed9949'
+VMESS_WSPATH='/4f2dee67-bc5a-435f-9667-4dc8c1ed9949-vmess'
+VLESS_WSPATH='/4f2dee67-bc5a-435f-9667-4dc8c1ed9949-vless'
 TROJAN_WSPATH='/trojan'
 SS_WSPATH='/shadowsocks'
 NEZHA_SERVER=''
